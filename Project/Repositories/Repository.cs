acf28cff58293395304f319fe409d3deb4dd30b7
Racheli weiss <75696575+racheliWeiss@users.noreply.github.com>
2021-05-02 17:24:00 +0300

    create jwt with token
    i crate jwt  and token to user with coonection to databace

---
+++ Project/Repositories/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Repositories
+{
+    public class Repository
+    {
+        private static Repository instance = new Repository();
+        private static readonly object mutex = new object();
+        private string connection = "";
+        public static Repository Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (mutex)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Repository();
+                        }
+
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Repository()
+        {
+            connection = @"server= Data Source = 82.166.177.109; Encrypt = False; Initial Catalog = MSB; Integrated Security = False; User ID = cpiLogin;Password: !qazXsw2";
+           
+
+        }
+    }
+}
